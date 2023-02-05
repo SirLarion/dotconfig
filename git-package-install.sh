c6941c108e8c05565cb1af54ff1bb63fc13645b7
sirlarion <miska.tammenpaa@gmail.com>
2023-02-05 18:04:32 +0200

    sync

--- git-package-install.sh
+++ git-package-install.sh
@@ -7,6 +7,7 @@ cd $DOWNLOADS
 # AUR packages
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 git clone https://aur.archlinux.org/spotify.git
+git clone https://aur.archlinux.org/minecraft-launcher.git
 
 # GH packages
 git clone https://github.com/davatorium/rofi
