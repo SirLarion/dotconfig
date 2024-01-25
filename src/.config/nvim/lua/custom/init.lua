7c97f581b8aa81bd9af8e62fcd4de695736f51e7
Miska Tammenpää <miska.tammenpaa@hoxhunt.com>
2024-01-25 16:52:41 +0200

    fiddle with nvimtree

--- src/.config/nvim/lua/custom/init.lua
+++ src/.config/nvim/lua/custom/init.lua
@@ -5,103 +5,106 @@ vim.opt.shiftwidth = 2
 
 vim.opt_global.textwidth = 80
 
+vim.g.toggle_theme_icon = ""
+
+
 -- Open nvim-tree on startup with some twists
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function(data)
-		-- buffer is a real file on the disk
-		local real_file = vim.fn.filereadable(data.file) == 1
+  callback = function(data)
+    -- buffer is a real file on the disk
+    local real_file = vim.fn.filereadable(data.file) == 1
 
-		-- buffer is a [No Name]
-		local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+    -- buffer is a [No Name]
+    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-		if not real_file and not no_name then
-			return
-		end
+    if not real_file and not no_name then
+      return
+    end
 
-		require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
-		-- open the tree, find the file but don't focus it
-	end,
+    require("nvim-tree.api").tree.toggle({ focus = false, find_file = true })
+    -- open the tree, find the file but don't focus it
+  end,
 })
 
 -- Close nvim-tree on last buffer close, courtesy of
 -- https://github.com/ppwwyyxx
 vim.api.nvim_create_autocmd("QuitPre", {
-	callback = function()
-		local invalid_win = {}
-		local wins = vim.api.nvim_list_wins()
-		for _, w in ipairs(wins) do
-			local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
-			if bufname:match("NvimTree_") ~= nil then
-				table.insert(invalid_win, w)
-			end
-		end
-		if #invalid_win == #wins - 1 then
-			-- Should quit, so we close all invalid windows.
-			for _, w in ipairs(invalid_win) do
-				vim.api.nvim_win_close(w, true)
-			end
-		end
-	end,
+  callback = function()
+    local invalid_win = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") ~= nil then
+        table.insert(invalid_win, w)
+      end
+    end
+    if #invalid_win == #wins - 1 then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(invalid_win) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end,
 })
 
 -- Use internal formatting for bindings like gq.
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		vim.bo[args.buf].formatexpr = nil
-	end,
+  callback = function(args)
+    vim.bo[args.buf].formatexpr = nil
+  end,
 })
 
 -- Format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		vim.lsp.buf.format()
-	end,
+  callback = function()
+    vim.lsp.buf.format()
+  end,
 })
 
 -- Function to check if a floating dialog exists and if not
 -- then check for diagnostics under the cursor
 -- https://neovim.discourse.group/t/how-to-show-diagnostics-on-hover/3830/2
 function OpenDiagnosticIfNoFloat()
-	for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-		if vim.api.nvim_win_get_config(winid).zindex then
-			return
-		end
-	end
-	-- THIS IS FOR BUILTIN LSP
-	vim.diagnostic.open_float(0, {
-		scope = "cursor",
-		focusable = false,
-		close_events = {
-			"CursorMoved",
-			"CursorMovedI",
-			"BufHidden",
-			"InsertCharPre",
-			"WinLeave",
-		},
-	})
+  for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if vim.api.nvim_win_get_config(winid).zindex then
+      return
+    end
+  end
+  -- THIS IS FOR BUILTIN LSP
+  vim.diagnostic.open_float(0, {
+    scope = "cursor",
+    focusable = false,
+    close_events = {
+      "CursorMoved",
+      "CursorMovedI",
+      "BufHidden",
+      "InsertCharPre",
+      "WinLeave",
+    },
+  })
 end
 
 -- Show diagnostics under the cursor when holding position
 vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
-	pattern = "*",
-	command = "lua OpenDiagnosticIfNoFloat()",
-	group = "lsp_diagnostics_hold",
+  pattern = "*",
+  command = "lua OpenDiagnosticIfNoFloat()",
+  group = "lsp_diagnostics_hold",
 })
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 -- custom version of https://github.com/neovim/nvim-lspconfig
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "<space>d", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-	end,
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<space>d", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  end,
 })
--- src/.config/nvim/lua/custom/plugins.lua
+++ src/.config/nvim/lua/custom/plugins.lua
@@ -1,92 +1,92 @@
 --@type NvPluginSpec[]
 local plugins = {
-	{
-		"christoomey/vim-tmux-navigator",
-		lazy = false,
-	},
-	{
-		"stevearc/conform.nvim",
-		lazy = false,
-		config = function()
-			require("custom.configs.format")
-		end,
-	},
-	{
-		"LhKipp/nvim-nu",
-		ft = "nu",
-		config = function()
-			require("nu").setup()
-			vim.g.nu_auto_format = 1
-		end,
-	},
-	{
-		"rinx/nvim-ripgrep",
-		config = function()
-			require("nvim-ripgrep").setup()
-		end,
-		lazy = false,
-	},
-	{
-		"mg979/vim-visual-multi",
-		init = function()
-			vim.g.VM_maps = {
-				["Find Under"] = "<C-d>",
-				["Find Subword Under"] = "<C-d>",
-				["Run Normal"] = "jj",
-			}
-		end,
-		lazy = false,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			indent = { enable = true, disable = { "dart" } },
-		},
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = {
-			filters = { custom = { "^.git$" } },
-		},
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		lazy = false,
-	},
-	{
-		"nvim-telescope/telescope-fzy-native.nvim",
-		lazy = false,
-		config = function()
-			require("telescope").load_extension("fzy_native")
-		end,
-	},
-	{
-		"andrewferrier/wrapping.nvim",
-		config = function()
-			require("wrapping").setup({
-				create_keymaps = false,
-				auto_set_mode_filetype_allowlist = {
-					"latex",
-					"markdown",
-					"tex",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-		lazy = false,
-	},
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "stevearc/conform.nvim",
+    lazy = false,
+    config = function()
+      require("custom.configs.format")
+    end,
+  },
+  {
+    "LhKipp/nvim-nu",
+    ft = "nu",
+    config = function()
+      require("nu").setup()
+      vim.g.nu_auto_format = 1
+    end,
+  },
+  {
+    "rinx/nvim-ripgrep",
+    config = function()
+      require("nvim-ripgrep").setup()
+    end,
+    lazy = false,
+  },
+  {
+    "mg979/vim-visual-multi",
+    init = function()
+      vim.g.VM_maps = {
+        ["Find Under"] = "<C-d>",
+        ["Find Subword Under"] = "<C-d>",
+        ["Run Normal"] = "jj",
+      }
+    end,
+    lazy = false,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      indent = { enable = true, disable = { "dart" } },
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require('custom.configs.nvimtree')
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+  },
+  {
+    "nvim-telescope/telescope-fzy-native.nvim",
+    lazy = false,
+    config = function()
+      require("telescope").load_extension("fzy_native")
+    end,
+  },
+  {
+    "andrewferrier/wrapping.nvim",
+    config = function()
+      require("wrapping").setup({
+        create_keymaps = false,
+        auto_set_mode_filetype_allowlist = {
+          "latex",
+          "markdown",
+          "tex",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+    lazy = false,
+  },
 }
 
 return plugins
