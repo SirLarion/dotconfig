66d02f66420893d4b25360f44f01625fd4c0062b
sirlarion <miska.tammenpaa@gmail.com>
2024-01-26 10:40:59 +0200

    finalize nvimtree keybindings

--- src/.config/nvim/lua/custom/configs/nvimtree.lua
+++ src/.config/nvim/lua/custom/configs/nvimtree.lua
@@ -1,122 +1,87 @@
 local nvimTree = require("nvim-tree")
 local api = require("nvim-tree.api")
 
-local function edit_or_open()
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    -- expand or collapse folder
-    api.node.open.edit()
-  else
-    -- open file
-    api.node.open.edit()
-    -- Close the tree if file was opened
-    api.tree.close()
-  end
-end
-
--- open as vsplit on current node
-local function vsplit_preview()
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    -- expand or collapse folder
-    api.node.open.edit()
-  else
-    -- open file as vsplit
-    api.node.open.vertical()
-  end
-
-  -- Finally refocus on tree if it was lost
-  api.tree.focus()
-end
-
-
--- vim.api.nvim_create_autocmd('BufEnter', ca)
-
 local options = {
-  -- on_attach = function()
-  --   vim.keymap.set("n", "l", edit_or_open)
-  --   vim.keymap.set("n", "L", vsplit_preview)
-  --   vim.keymap.set("n", "h", api.tree.close)
-  --   vim.keymap.set("n", "H", api.tree.collapse_all)
-  -- end,
-  filters = {
-    dotfiles = false,
-    exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
-    custom = { custom = { "^.git$" } }
-  },
-  disable_netrw = true,
-  hijack_netrw = true,
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
-  sync_root_with_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_root = false,
-  },
-  view = {
-    centralize_selection = true,
-    adaptive_size = false,
-    side = "left",
-    width = 30,
-    preserve_window_proportions = true,
-  },
-  git = {
-    enable = true,
-    ignore = true,
-  },
-  filesystem_watchers = {
-    enable = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
-  },
-  renderer = {
-    indent_width = 1,
-    root_folder_label = false,
-    highlight_git = false,
-    highlight_opened_files = "none",
+	on_attach = function(bufnr)
+		vim.keymap.set("n", "l", api.node.open.edit, { buffer = bufnr })
+		vim.keymap.set("n", "H", api.tree.collapse_all, { buffer = bufnr })
+		vim.keymap.set("n", "h", api.node.navigate.parent_close, { buffer = bufnr })
+	end,
+	filters = {
+		dotfiles = false,
+		exclude = { vim.fn.stdpath("config") .. "/lua/custom" },
+		custom = { "^.git$" },
+	},
+	disable_netrw = true,
+	hijack_netrw = true,
+	hijack_cursor = true,
+	hijack_unnamed_buffer_when_opening = false,
+	sync_root_with_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_root = false,
+	},
+	view = {
+		centralize_selection = true,
+		adaptive_size = false,
+		side = "left",
+		width = 30,
+		preserve_window_proportions = true,
+	},
+	git = {
+		enable = true,
+	},
+	filesystem_watchers = {
+		enable = true,
+	},
+	actions = {
+		open_file = {
+			resize_window = true,
+		},
+	},
+	renderer = {
+		indent_width = 1,
+		root_folder_label = false,
+		highlight_git = false,
+		highlight_opened_files = "none",
 
-    indent_markers = {
-      enable = false,
-    },
+		indent_markers = {
+			enable = false,
+		},
 
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = false,
-      },
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = false,
+			},
 
-      glyphs = {
-        default = "󰈚",
-        symlink = "",
-        folder = {
-          default = "",
-          empty = "",
-          empty_open = "",
-          open = "",
-          symlink = "",
-          symlink_open = "",
-          arrow_open = "",
-          arrow_closed = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-  },
+			glyphs = {
+				default = "󰈚",
+				symlink = "",
+				folder = {
+					default = "",
+					empty = "",
+					empty_open = "",
+					open = "",
+					symlink = "",
+					symlink_open = "",
+					arrow_open = "",
+					arrow_closed = "",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
+	},
 }
 
 nvimTree.setup(options)
