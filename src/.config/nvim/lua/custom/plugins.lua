43e57c023a4897fac6d9f1a65f2fbc085d1edaff
sirlarion <miska.tammenpaa@gmail.com>
2024-02-01 12:54:27 +0200

    add harpoon

--- src/.config/nvim/lua/custom/plugins.lua
+++ src/.config/nvim/lua/custom/plugins.lua
@@ -87,6 +87,15 @@ local plugins = {
 		end,
 		lazy = false,
 	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		config = function()
+			require("harpoon").setup({})
+			require("telescope").load_extension("harpoon")
+		end,
+		lazy = false,
+	},
 }
 
 return plugins
