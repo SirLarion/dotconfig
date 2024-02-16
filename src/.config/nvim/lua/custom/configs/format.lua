df85c49288daa280b77eaa4f7968d7a02b4231d0
sirlarion <miska.tammenpaa@gmail.com>
2024-02-16 11:35:56 +0200

    update pkgdb

--- src/.config/nvim/lua/custom/configs/format.lua
+++ src/.config/nvim/lua/custom/configs/format.lua
@@ -1,24 +1,25 @@
 local conform = require("conform")
 
 conform.setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "prettierd" },
-    javascriptreact = { "prettierd" },
-    typescript = { "prettierd" },
-    typescriptreact = { "prettierd" },
-    css = { "prettierd" },
-    html = { "prettierd" },
-    c = { "clang_format" },
-    cpp = { "clang_format" },
-    arduino = { "clang_format" },
-    bash = { "beautysh" },
-    xml = { "xmlformat" },
-    opml = { "xmlformat" },
-    rs = { "rustfmt" },
-  },
-  format_on_save = {
-    timeout_ms = 200,
-    lsp_fallback = true,
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		json = { "prettierd" },
+		css = { "prettierd" },
+		html = { "prettierd" },
+		c = { "clang_format" },
+		cpp = { "clang_format" },
+		arduino = { "clang_format" },
+		bash = { "beautysh" },
+		xml = { "xmlformat" },
+		opml = { "xmlformat" },
+		rs = { "rustfmt" },
+	},
+	format_on_save = {
+		timeout_ms = 200,
+		lsp_fallback = true,
+	},
 })
--- src/.config/nvim/lua/custom/configs/lspconfig.lua
+++ src/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,62 +3,62 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = { "cssls", "rust_analyzer", "texlab", "html", "clangd", "lemminx", "arduino_language_server" }
+local servers = { "cssls", "rust_analyzer", "texlab", "html", "clangd", "lemminx", "arduino_language_server", "jsonls" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-  })
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
 -- Fix for multiple definitions in React when using gd
 -- https://github.com/typescript-language-server/typescript-language-server/issues/216
 local function filter(arr, fn)
-  if type(arr) ~= "table" then
-    return arr
-  end
+	if type(arr) ~= "table" then
+		return arr
+	end
 
-  local filtered = {}
-  for k, v in pairs(arr) do
-    if fn(v, k, arr) then
-      table.insert(filtered, v)
-    end
-  end
+	local filtered = {}
+	for k, v in pairs(arr) do
+		if fn(v, k, arr) then
+			table.insert(filtered, v)
+		end
+	end
 
-  return filtered
+	return filtered
 end
 
 local function filterDts(value)
-  local uri = value.uri ~= nil and value.uri or value.targetUri
-  if uri == nil then
-    uri = value.filename ~= nil and value.filename or ""
-  end
-  return string.match(uri, "%.d.ts") == nil
+	local uri = value.uri ~= nil and value.uri or value.targetUri
+	if uri == nil then
+		uri = value.filename ~= nil and value.filename or ""
+	end
+	return string.match(uri, "%.d.ts") == nil
 end
 
 lspconfig.tsserver.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  init_options = {
-    preferences = {
-      disableSuggestions = true,
-    }
-  },
-  handlers = {
-    ["textDocument/definition"] = function(err, result, method, ...)
-      if vim.tbl_islist(result) and #result > 1 then
-        local filtered_result = filter(result, filterDts)
-        return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
-      end
+	on_attach = on_attach,
+	capabilities = capabilities,
+	init_options = {
+		preferences = {
+			disableSuggestions = true,
+		},
+	},
+	handlers = {
+		["textDocument/definition"] = function(err, result, method, ...)
+			if vim.tbl_islist(result) and #result > 1 then
+				local filtered_result = filter(result, filterDts)
+				return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+			end
 
-      vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
-    end,
-  },
+			vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+		end,
+	},
 })
 
 vim.lsp.handlers["textDocument/references"] = function()
-  require("telescope.builtin").lsp_references({
-    initial_mode = "normal"
-  })
+	require("telescope.builtin").lsp_references({
+		initial_mode = "normal",
+	})
 end
